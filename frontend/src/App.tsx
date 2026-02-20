ebd201571ca032b372b7b52443d779a68de650a4
Angekarara <mbabange2020@gmail.com>
2026-02-20 20:59:56 +0200

    feat: create new item form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import Forms from "./components/forms/Forms";
 import ItemsFilter from "./components/ItemsFilter";
 import { Navbar } from "./components/navigations/Navbar";
+import Modal from "./components/shared/Modal";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className="min-h-screen bg-[#fdf5ea]">
-      <Navbar />
+      <Navbar setIsModalOpen={setIsModalOpen} />
       <ItemsFilter />
-      <Forms />
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Add New Item"
+      >
+        <Forms />
+      </Modal>
     </div>
   );
 }
