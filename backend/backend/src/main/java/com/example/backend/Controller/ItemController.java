7af802384e09dfbe66667856d4807c8590d1cd34
Angekarara <mbabange2020@gmail.com>
2026-02-23 15:53:25 +0200

    chore: create item controller

---
+++ backend/backend/src/main/java/com/example/backend/Controller/ItemController.java
@@ -0,0 +1,37 @@
+package com.example.backend.Controller;
+
+import com.example.backend.Model.Item;
+import com.example.backend.Repo.ItemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ItemController {
+
+    @Autowired
+    private ItemRepo itemRepo;
+
+    @GetMapping(value = "/items")
+    public List<Item> getItem(){
+        return itemRepo.findAll();
+    }
+    @PostMapping(value = "/save")
+    public String saveItem(@RequestBody Item item){
+        itemRepo.save(item);
+        return "saved..";
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public String updateItem(@PathVariable long id, @RequestBody Item item){
+        Item updatedItem= itemRepo.findById(id).get();
+        updatedItem.setCategory(item.getCategory());
+        updatedItem.setItemName(item.getItemName());
+        updatedItem.setOwner(item.getOwner());
+        updatedItem.setExpirationDate(item.getExpirationDate());
+
+        itemRepo.save(updatedItem);
+        return "Updated..";
+}
+}
